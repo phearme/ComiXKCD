2380d17b1f49b377c7079cf1fc24cc9963b2b33c
phearme <raphael.cohen@gmail.com>
2018-09-08 15:13:30 +0300

    about

--- app/src/main/java/com/phearme/comixkcd/MainActivity.java
+++ app/src/main/java/com/phearme/comixkcd/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity implements MainViewModel.IMa
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about) {
             return true;
         }
         return super.onOptionsItemSelected(item);
